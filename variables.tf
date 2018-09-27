4af5594fe3c4c502a3e14c8d6b8a7d8143f401fe
root <root@ip-172-31-85-2.ec2.internal>
2018-09-27 18:34:07 +0000

    created new feature branch for aws eks with terraform

--- variables.tf
+++ variables.tf
@@ -8,7 +8,7 @@ variable "secret_key" {
 
 variable "aws_region" {
   description = "AWS region to launch servers."
-  default     = "us-west-2"
+  default     = "us-east-1"
 }
 
 variable "cidr_block" {
@@ -25,10 +25,6 @@ variable "eks_cluster_name" {
   default     = "terra"
 }
 
-variable "identifier" {
-  description = "Identifier for DB"
-  default     = "terra-db"
-}
 
 variable "storage_type" {
   description = "Type of the storage ssd or magnetic"
@@ -44,30 +40,3 @@ variable "allocated_storage" {
   }
 }
 
-variable "db_engine" {
-  description = " DB engine"
-  default     = "postgres"
-}
-
-variable "engine_version" {
-  description = "DB engine version"
-  default     = "9.6.6"
-}
-
-variable "instance_class" {
-  description = "mashine type to be used"
-
-  default = {
-    prod = "db.t2.micro"
-    dev  = "db.t2.large"
-  }
-}
-
-variable "db_username" {
-  description = "db admin user"
-  default     = "root"
-}
-
-variable "db_password" {
-  description = "password, provide through your tfvars file"
-}
